3bdd1b4d179f821c7438c906a697a846b235c178
Olgun AYDIN <olgunaydinn@gmail.com>
2023-02-19 19:48:41 +0100

    topsis function for modified version of topsis added

---
+++ pyDecision/algorithm/topsis_modified.py
@@ -0,0 +1,66 @@
+###############################################################################
+
+# Required Libraries
+import matplotlib.pyplot as plt
+import numpy as np
+
+###############################################################################
+
+# Function: Rank 
+def ranking(flow):    
+    rank_xy = np.zeros((flow.shape[0], 2))
+    for i in range(0, rank_xy.shape[0]):
+        rank_xy[i, 0] = 0
+        rank_xy[i, 1] = flow.shape[0]-i           
+    for i in range(0, rank_xy.shape[0]):
+        plt.text(rank_xy[i, 0],  rank_xy[i, 1], 'a' + str(int(flow[i,0])), size = 12, ha = 'center', va = 'center', bbox = dict(boxstyle = 'round', ec = (0.0, 0.0, 0.0), fc = (0.8, 1.0, 0.8),))
+    for i in range(0, rank_xy.shape[0]-1):
+        plt.arrow(rank_xy[i, 0], rank_xy[i, 1], rank_xy[i+1, 0] - rank_xy[i, 0], rank_xy[i+1, 1] - rank_xy[i, 1], head_width = 0.01, head_length = 0.2, overhang = 0.0, color = 'black', linewidth = 0.9, length_includes_head = True)
+    axes = plt.gca()
+    axes.set_xlim([-1, +1])
+    ymin = np.amin(rank_xy[:,1])
+    ymax = np.amax(rank_xy[:,1])
+    if (ymin < ymax):
+        axes.set_ylim([ymin, ymax])
+    else:
+        axes.set_ylim([ymin-1, ymax+1])
+    plt.axis('off')
+    plt.show() 
+    return
+
+
+# Function: TOPSIS modified
+
+def topsis_method_modified(dataset, weights, criterion_type, graph=True):
+    X = np.copy(df_array)
+    w = np.copy(weights)
+    sum_cols = np.sum(X * X, axis=0)
+    sum_cols = sum_cols ** (1 / 2)
+    r_ij = X / sum_cols
+    v_ij = r_ij * w
+    p_ideal_A = np.zeros(X.shape[1])
+    n_ideal_A = np.zeros(X.shape[1])
+    for i in range(0, dataset.shape[1]):
+        if (criterion_type[i] == 'max'):
+            p_ideal_A[i] = np.max(v_ij[:, i])
+            n_ideal_A[i] = np.min(v_ij[:, i])
+        else:
+            p_ideal_A[i] = np.min(v_ij[:, i])
+            n_ideal_A[i] = np.max(v_ij[:, i])
+
+
+    p_s_ij = abs(v_ij - p_ideal_A)
+    p_s_ij = np.max(p_s_ij, 1)
+    n_s_ij = abs(v_ij - n_ideal_A)
+    n_s_ij = np.max(n_s_ij, 1)
+
+    c_i = n_s_ij / (p_s_ij + n_s_ij)
+
+    if (graph == True):
+        flow = np.copy(c_i)
+        flow = np.reshape(flow, (c_i.shape[0], 1))
+        flow = np.insert(flow, 0, list(range(1, c_i.shape[0] + 1)), axis=1)
+        flow = flow[np.argsort(flow[:, 1])]
+        flow = flow[::-1]
+        # ranking(flow)
+    return r_ij, p_s_ij, n_s_ij, c_i
